0795e5fc3bdeaeb3f1f1b54e1bb4b48f4b4fba04
Jonas <jhraun@gmail.com>
2022-04-21 14:13:45 +0200

    torsdag

---
+++ Shelter/Shared/Models/BookingItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Shelter.Shared.Models
+{
+    [BsonIgnoreExtraElements]
+    public class BookingItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? MongoId { get; set; }
+
+        [BsonElement("properties")]
+        public ShelterProperty property { get; set; }
+
+        public BookingItem()
+
+        {
+        }
+
+
+    }
+
+}
