f4500eb8a9248268dce58e3e2682bad5ff6e3f97
Jonas <jhraun@gmail.com>
2022-04-21 10:17:42 +0200

    propertyklasse

--- Shelter/Shared/Models/ShelterItem.cs
+++ Shelter/Shared/Models/ShelterItem.cs
@@ -8,32 +8,22 @@ using MongoDB.Bson;
 
 namespace Shelter.Shared.Models
 {
+    [BsonIgnoreExtraElements]
     public class ShelterItem
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? MongoId { get; set; }
 
-        [BsonElement("type")]
-        public string Navn { get; set; }
+        [BsonElement("properties")]
+        public ShelterProperty property { get; set; }
 
-
-        [BsonElement("properties.beskrivels")]
-        public string Beskrivelse { get; set; }
-
-
-        [BsonElement("properties.antal_pl")] //properties.x
-        public int AntalPladser { get; set; }
-
-       /* public ShelterItem(string navn = "shelter", string beskrivelse = "hej", int antalpladser = 1)
-        {
-            Navn = navn;
-            Beskrivelse = beskrivelse;
-            AntalPladser = antalpladser;
-        }*/
        public ShelterItem()
+        
         {
-
         }
+       
+
     }
+
 }
---
+++ Shelter/Shared/Models/ShelterProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Shelter.Shared.Models
+{
+    [BsonIgnoreExtraElements]
+    public class ShelterProperty
+    {
+        [BsonElement("navn")]
+        public string? Navn { get; set; }
+        [BsonElement("beskrivels")]
+        public string? Beskrivelse { get; set; }
+        [BsonElement("antal_pl")]
+        public int AntalPladser { get; set; }
+    }
+}
